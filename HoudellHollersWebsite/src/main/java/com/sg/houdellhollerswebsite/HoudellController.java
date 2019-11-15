4ce7083248326cfa30a744be93d4e2ad81627f2f
Austin Taylor Mann <Fancysause@gmail.com>
2019-11-15 12:03:00 -0500

    controller changes

--- HoudellHollersWebsite/src/main/java/com/sg/houdellhollerswebsite/HoudellController.java
+++ HoudellHollersWebsite/src/main/java/com/sg/houdellhollerswebsite/HoudellController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping({"/hello"})
 public class HoudellController {
         
     @RequestMapping(value = "/viewhome", method = RequestMethod.GET)
