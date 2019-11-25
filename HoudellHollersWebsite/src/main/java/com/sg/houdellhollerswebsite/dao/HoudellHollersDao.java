3219c79e73fec92af479d98bd23fdd6baa35ee0f
Austin Taylor Mann <Fancysause@gmail.com>
2019-11-25 15:41:03 -0500

    started adding dao methods

---
+++ HoudellHollersWebsite/src/main/java/com/sg/houdellhollerswebsite/dao/HoudellHollersDao.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.houdellhollerswebsite.dao;
+
+import java.util.List;
+
+/**
+ *
+ * @author austinmann
+ */
+public class HoudellHollersDao {
+//==============================================================================
+//PICTURE METHODS
+//==============================================================================
+    public List<Picture> getAllPictures();
+//==============================================================================
+//VIDEO METHODS
+//==============================================================================
+    public List<Video> getAllVideos();
+//==============================================================================
+//SHOW METHODS
+//==============================================================================
+    public List<Show> getAllShows();
+//==============================================================================
+//CONTACT METHODS
+//==============================================================================
+    public List<Contact> getAllContacts();
+}
